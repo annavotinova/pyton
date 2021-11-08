89051b7b3db53dd2718ee4bf06fe359919e92697
annavotinova <votinova.an9@gmail.com>
2021-11-08 19:26:38 +0300

    Homework_3

---
+++ Homework_3_3.py
@@ -0,0 +1,8 @@
+#Задание №2
+
+def my_func (var_1, var_2, var_3):
+    sum_1 = var_1 + var_2
+    sum_2 = var_2 + var_3
+    sum_3 = var_1 + var_3
+    print(max(sum_1, sum_2, sum_3))
+my_func (1,2,3)
---
+++ Homework_3_4.py
@@ -0,0 +1,4 @@
+def my_func (x,y):
+    c = x**y
+    print(c)
+my_func (2.5,-1)
