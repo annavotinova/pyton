b36e1bb5aad2c1ee4fc21f5c2977606f74051a28
annavotinova <votinova.an9@gmail.com>
2021-11-08 19:22:50 +0300

    Del

--- Homework_3_2.py
+++
@@ -1,5 +0,0 @@
-#Задание №2
-
-def inf_men(name, surname, date_brth, city, email, phone):
-    print(f'{name} {surname} {date_brth} {city} {email} {phone}')
-inf_men(name='Анна', surname='Вотинова', date_brth='16/05/1987', city='Сочи', email='vot@gmail.com', phone='79091132314')
--- Homework_3_4.py
+++
@@ -1,4 +0,0 @@
-def my_func (x,y):
-    c = x**y
-    print(c)
-my_func (2.5,-1)
